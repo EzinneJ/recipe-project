47c890fc5889ca81daac0a1b12e66d31bd87def5
EzinneJ <chimejoyez@gmail.com>
2022-06-29 09:47:50 +0100

    completing assignment

---
+++ src/main/java/ezinne/springframework/controllers/IndexController.java
@@ -0,0 +1,11 @@
+package ezinne.springframework.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class IndexController {
+
+    @RequestMapping({"","/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
