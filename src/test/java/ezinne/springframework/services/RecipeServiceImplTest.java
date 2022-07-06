15dbdf1588467b368a4ee45884a754e6bee8a920
EzinneJ <chimejoyez@gmail.com>
2022-07-06 09:32:14 +0100

    tests and some classes plus some html templating

--- src/test/java/ezinne/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/ezinne/springframework/services/RecipeServiceImplTest.java
@@ -8,27 +8,47 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class RecipeServiceImplTest {
-RecipeServiceImpl recipeService;
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
 
     @Mock
     RecipeRepository recipeRepository;
 
+
     @BeforeEach
-    void setUp() {
+    public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
     @Test
-    void getRecipes() {
+    public void getRecipeByIdTest() throws Exception {
         Recipe recipe = new Recipe();
-        HashSet recipesData = new HashSet<>();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull(recipeReturned, "Null recipe returned");
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipesTest() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
         recipesData.add(recipe);
 
         when(recipeService.getRecipes()).thenReturn(recipesData);
@@ -37,5 +57,7 @@ RecipeServiceImpl recipeService;
 
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
     }
+
 }
